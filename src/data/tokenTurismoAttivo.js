326e6f06905abe3fb45763c907078518115c7f00
Mauro Marini <mauro.marini97@gmail.com>
2020-07-03 09:41:10 +0200

    Update tokenTurismoAttivo.js

--- src/data/tokenTurismoAttivo.js
+++ src/data/tokenTurismoAttivo.js
@@ -1,4 +1,4 @@
-/* this is not intended for production app */
+/* to run the app you need to obtain the access token for the sin2 API */
 
 let TurismoAttivoAuth ={
     access: 'REPLACE_WITH_USER_TOKEN',
